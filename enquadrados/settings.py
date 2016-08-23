7910656e18b2d46451428677894b1d6cd383eace
Guilherme Baptista <guisgb13@gmail.com>
2016-08-23 02:36:09 -0300

    Whitelisting any domain (cors)

--- enquadrados/settings.py
+++ enquadrados/settings.py
@@ -57,9 +57,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = [
-    'localhost:8001'
-]
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'enquadrados.urls'
 
